92e178d47d24e69af25d478b34905aa5a89d040f
dinellipaulo <pgsdinelli@gmail.com>
2024-11-11 16:25:53 -0300

    Project created

---
+++ src/main/java/com/dinellipaulo/gamesrank/GamesrankApplication.java
@@ -0,0 +1,13 @@
+package com.dinellipaulo.gamesrank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesrankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesrankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dinellipaulo/gamesrank/GamesrankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinellipaulo.gamesrank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesrankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
